29fc411d9430a3298571e3e2dda3dd6bee9bbd78
user <user@users-MacBook-Pro.local>
2020-10-16 22:23:35 -0700

    Updates

---
+++ precache-manifest.8b8769ae03c7d387078cd1a730967464.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d03ba1df91e2800a0ac1002b7de8d4",
+    "url": "/Abdifatah-portfolio/index.html"
+  },
+  {
+    "revision": "52960e424cff6e7f9729",
+    "url": "/Abdifatah-portfolio/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "4b9ebf686fd274305b63",
+    "url": "/Abdifatah-portfolio/static/css/main.ecb2b038.chunk.css"
+  },
+  {
+    "revision": "52960e424cff6e7f9729",
+    "url": "/Abdifatah-portfolio/static/js/2.31d189ac.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Abdifatah-portfolio/static/js/2.31d189ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b9ebf686fd274305b63",
+    "url": "/Abdifatah-portfolio/static/js/main.49e12570.chunk.js"
+  },
+  {
+    "revision": "f82ae67edca839e4e4e4",
+    "url": "/Abdifatah-portfolio/static/js/runtime-main.06d0e3cd.js"
+  },
+  {
+    "revision": "fb4274b65fd17f44bcffa0710db435a8",
+    "url": "/Abdifatah-portfolio/static/media/1.fb4274b6.png"
+  },
+  {
+    "revision": "1648f56c411a34e1ada981c0ddf3eb08",
+    "url": "/Abdifatah-portfolio/static/media/2.1648f56c.png"
+  },
+  {
+    "revision": "cf038270078474c559e19a1b5d880bc1",
+    "url": "/Abdifatah-portfolio/static/media/4.cf038270.jpg"
+  },
+  {
+    "revision": "efad342ce11d51f4a96efdae4795839e",
+    "url": "/Abdifatah-portfolio/static/media/7.efad342c.png"
+  },
+  {
+    "revision": "3dea0e60cbacef50bcddc3972f273b73",
+    "url": "/Abdifatah-portfolio/static/media/8.3dea0e60.png"
+  },
+  {
+    "revision": "c0f649e64ff4e3897562730cabb55733",
+    "url": "/Abdifatah-portfolio/static/media/9.c0f649e6.png"
+  },
+  {
+    "revision": "628af5e384aab9971b7929110d048f73",
+    "url": "/Abdifatah-portfolio/static/media/abdi.628af5e3.jpg"
+  },
+  {
+    "revision": "e21b18e158da85fe72ea22f1d4a7df24",
+    "url": "/Abdifatah-portfolio/static/media/azure.e21b18e1.jpg"
+  },
+  {
+    "revision": "88d38d998b1ef7642e5245798643d31f",
+    "url": "/Abdifatah-portfolio/static/media/blobStorage.88d38d99.png"
+  },
+  {
+    "revision": "3da254e6f21c29233971b6189020e769",
+    "url": "/Abdifatah-portfolio/static/media/dataCamp.3da254e6.png"
+  },
+  {
+    "revision": "5f79cd875751394bbc2e9b8df36cefa6",
+    "url": "/Abdifatah-portfolio/static/media/exampleImage.5f79cd87.png"
+  },
+  {
+    "revision": "141eaa1af1af959c4d2fb9b1a660d449",
+    "url": "/Abdifatah-portfolio/static/media/firebaseAuth.141eaa1a.png"
+  },
+  {
+    "revision": "d6c7579fc9f4b07ac380601248470505",
+    "url": "/Abdifatah-portfolio/static/media/floop.d6c7579f.png"
+  },
+  {
+    "revision": "202275cbd1a5d74245506f8ff8a16cf6",
+    "url": "/Abdifatah-portfolio/static/media/floop01.202275cb.PNG"
+  },
+  {
+    "revision": "bedac015c3d849bf38b173b34138a58d",
+    "url": "/Abdifatah-portfolio/static/media/linkedIn.bedac015.png"
+  },
+  {
+    "revision": "aa9661dbd3fd5b158aac086129f9741e",
+    "url": "/Abdifatah-portfolio/static/media/mementoCRM.aa9661db.jpg"
+  },
+  {
+    "revision": "054e6b2ca96ec5041c8570206dec6cb5",
+    "url": "/Abdifatah-portfolio/static/media/mementoLogo.054e6b2c.png"
+  },
+  {
+    "revision": "5c3335505d7cd9af6c6bb3fb0e559621",
+    "url": "/Abdifatah-portfolio/static/media/postmanPutRequest.5c333550.png"
+  },
+  {
+    "revision": "e5114a1ebfc875ed3dbbd83ccfdc95a9",
+    "url": "/Abdifatah-portfolio/static/media/potOgold01.e5114a1e.png"
+  },
+  {
+    "revision": "c041605849a86c9ffb13492071490b09",
+    "url": "/Abdifatah-portfolio/static/media/putRequestLogic.c0416058.png"
+  },
+  {
+    "revision": "17dbd18dfb87716d272b48bdff8f7d63",
+    "url": "/Abdifatah-portfolio/static/media/realTimeDB.17dbd18d.png"
+  },
+  {
+    "revision": "e6b4363f7e71aefe65c4ea1c9ec18c47",
+    "url": "/Abdifatah-portfolio/static/media/teamTreehouse01.e6b4363f.png"
+  }
+]);
